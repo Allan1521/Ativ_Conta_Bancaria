e55433444ce60325cfe5c2fda779a815204510e2
Allan <allan1521lima@gmail.com>
2024-09-20 22:36:03 -0300

    Ativ UC06 Exerc-03

--- Ativ_Conta_Bancaria/Program.cs
+++ Ativ_Conta_Bancaria/Program.cs
@@ -2,7 +2,12 @@
 
 using Allan.Ativ_Conta_Bancaria.Models;
 using System.Reflection.Metadata;
+
+class Program
 {
-
+    static void Main()
+    {
+        ContaEspecial Especial = new ContaEspecial("Allan", "0001-01.02.03-1", "100");
+    }
+      
 }
-
