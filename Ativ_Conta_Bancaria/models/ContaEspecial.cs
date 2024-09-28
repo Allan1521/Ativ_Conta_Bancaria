a83748ec5a5de3cebb633574d196095f8f6db46d
Allan <allan1521lima@gmail.com>
2024-09-27 21:51:10 -0300

    Ativ UC06 Exerc-03

--- Ativ_Conta_Bancaria/models/ContaEspecial.cs
+++ Ativ_Conta_Bancaria/models/ContaEspecial.cs
@@ -11,12 +11,40 @@ internal class ContaEspecial
 {
     public string TitularDaConta { get; set; }      
     public string NumerodaConta { get; set; }
+    public double Saldo { get; set; }
     public double Limite { get; set; }
 
-    public ContaEspecial(string titularDaConta, string numerodaConta, double limite)
+    public ContaEspecial(string titularDaConta, string numerodaConta, double Saldo,
+    double limite)
     {
         this.TitularDaConta = titularDaConta;
         this.NumerodaConta = numerodaConta;
         this.Limite = limite;
+        this.Saldo = Saldo;
+    }
+    public void Sacar(double valor)
+    {
+        if (valor <= this.Saldo + this.Limite)
+        {
+            this.Saldo -= valor;
+            Console.WriteLine($"Saque de R${valor} realizado com Sucesso. Saldo atual é: R${this.Saldo}");
+        }
+        else
+        {
+
+            Console.WriteLine("Saque não permitido. O valor inserido excede o saldo e o limite.");
+        }
+    }
+    //Retonar o método de exibir os dados Conta Especial
+    public string ExibirDadosConta()
+    {
+
+        return
+            $"Titular: {TitularDaConta}," +
+            $" Número da Conta: {NumerodaConta}," +
+            $" Saldo: R$ {Saldo}," +
+            $" Limite Disponivel: R$ {Limite}";
+
     }
 }
+
--- Ativ_Conta_Bancaria/models/ContaPoupanca.cs
+++ Ativ_Conta_Bancaria/models/ContaPoupanca.cs
@@ -12,11 +12,40 @@ namespace Allan.Ativ_Conta_Bancaria.Models
         public string TitularDaConta { get; set; }
         public string NumerodaConta { get; set; }
         public double Saldo { get; set; }
-        public DateTime DataAniversarioDaConta { get; set; }
+        public DateTime DataAniversarioDaConta = DateTime.Now;
 
-        public ContaPoupanca()
+        public ContaPoupanca() { }
+        public void Sacar(double valor)
         {
-            
+            if (valor <= this.Saldo)
+            {
+                this.Saldo = valor;
+                Console.WriteLine($"Saque de R${valor} realizado com Sucesso. Saldo atual é: R${this.Saldo}");
+            }
+            else
+            {
+                Console.WriteLine("Saque não permitido. valor inserido execede o Saldo");
+            }
         }
+        public ContaPoupanca(string TitularPoupanca, string NumeroContaPoupanca, double SaldoPoupanca)
+        {
+            this.TitularDaConta = TitularPoupanca;
+            this.NumerodaConta = NumeroContaPoupanca;
+            this.Saldo = SaldoPoupanca;
+
+        }
+
+        //Retonar o método de exibir os dados Conta Poupança
+        public string ExibirDadosConta()
+        {
+            return
+                $"Titular: {TitularDaConta}," +
+                $" Número da Conta: {NumerodaConta}, " +
+                $" Saldo: R$ {Saldo}, " +
+                $" Data de Aniversário: {DataAniversarioDaConta}";
+        }
+
     }
 }
+    
+
